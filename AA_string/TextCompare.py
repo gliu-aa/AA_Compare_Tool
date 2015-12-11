4f646c1fff4f63b94677a0399449a34a7828de24
gliu-aa <gliu@appannie.com>
2015-12-11 19:29:29 +0800

    edit

---
+++ AA_string/TextCompare.py
@@ -0,0 +1,1 @@
+from AA_constant import constantclass TextCompare:    """A utility class to compare text"""    @classmethod    def text_distance(cls, source, target):        pass    @classmethod    def text_different(cls, source, target):        pass
